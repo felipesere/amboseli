2131c3c34354d07b9fcdb4cda867404c738669b0
Felipe Sere <felipesere@gmail.com>
2018-11-26 22:31:15 +0000

    Add the Advent link after the first of December

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,17 +2,26 @@ import React from 'react'
 import style from './navigation.module.scss'
 import { Link } from 'gatsby'
 import cx from 'classnames'
+import moment from 'moment'
 
 export const NavBar = ({ center = false }) => {
   const className = cx(style.navbar, {
     [style.centered]: center,
   })
 
+
   return (
     <nav className={className}>
       <Link to="/">Home</Link>
       <Link to="/blog">Blog</Link>
       <Link to="/portfolio">Portfolio</Link>
+      {isXmas() && (<Link to="/advent">Advent</Link>)}
     </nav>
   )
+}
+
+const isXmas = () => {
+  const decemberFirst = moment('2018-12-01').startOf('date')
+  const now = moment()
+  return now.isAfter(decemberFirst) || (process.env.NODE_ENV !== 'production')
 }
